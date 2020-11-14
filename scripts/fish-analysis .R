03fd55588a1bb6d033e8424d19958ac59ec77445
hhochel <hhochel@uw.edu>
2020-11-13 17:58:29 -0800

    renaming the data file and updating the script

--- scripts/fish-analysis .R
+++ scripts/fish-analysis .R
@@ -1,6 +1,6 @@
 #reading in some data
 
-fish.data <- read.csv("data/Gaeta_etal_CLC_data.csv")
+fish.data <- read.csv("data/Gaeta_etal_CLC_data_1.csv")
 
 
 #building history
@@ -12,3 +12,4 @@ fish_data_cat <- fish.data %>%
   mutate(length_cat = ifelse(length > 300, "big", "small"))
 
 fish_data_cat_clean <- filter(fish_data_cat, scalelength > 1)
+
