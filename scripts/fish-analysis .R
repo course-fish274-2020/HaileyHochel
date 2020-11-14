949999275cd78789e35fc61f557096014b183c7c
hhochel <hhochel@uw.edu>
2020-11-13 17:41:01 -0800

    add categorical fish length column

---
+++ scripts/fish-analysis .R
@@ -0,0 +1,10 @@
+#reading in some data
+
+fish.data <- read.csv("data/Gaeta_etal_CLC_data.csv")
+
+
+#building history
+
+library(dplyr)
+fish_data_cat <- fish.data %>%
+  mutate(length_cat = ifelse(length > 200, "big", "small"))
